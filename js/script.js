d905693139e0f53c85806fd9fc6ccccadc42ba68
Arsamurzaev Deni <111310702+arsamurzaev@users.noreply.github.com>
2023-07-01 15:32:43 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 function clickBurger() {
   const button = document.querySelector(".header__burger-menu");
+  const wrapper = document.querySelector(".wrapper");
   const animItems = document.querySelector(".anim__items");
   button.addEventListener("click", () => {
     animItems.classList.add("active");
+    wrapper.style.display = "none";
   });
 }
 
